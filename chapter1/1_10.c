49f93080e5534e62bc85b1681bd40c3629644d8f
Nuolong <nuolongu@gmail.com>
2020-05-13 01:18:30 -0400

    added 1.10

--- chapter1/1_10.c
+++ chapter1/1_10.c
@@ -2,47 +2,63 @@
 
 #define MAXLINE     1000
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+// these are external variables, an alternative to automatic variables. this sets aside storage for them.
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getline1(void); // note: void must be used for explicitly empty list. discussed later
+void copy(void);
 
 main(){
-    int len, max;
-    char line[MAXLINE], longest[MAXLINE];
+    int len;
+    extern int max;         // declarations of external variables in the functions that want to use it
+    extern char longest[];  // a common practice to put all external variable definitions in the beginning
+                            // of the file, and so you can remove all further 'externs' in following funcs.
 
     max = 0;
-    while((len = getline(line, MAXLINE)) > 0)
+    while((len = getline1()) > 0)
         if(len > max){
             max = len;
-            copy(longest, line);
+            copy();
         }
-    if(max > 0){
+    if(max > 0)
         printf("%s\n", longest);
-        printf("Length of this line: %d\n", max); // 1-16
-
-    }
+    
     return 0;
 }
 
 /* read a line into s and return its length */
-int getline(char s[], int lim){
+int getline1(void){
     int c, i;
+    extern char line[]
 
-    for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
+    for(i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
     if(c == '\n'){
-        s[i] = c;
+        line[i] = c;
         ++i;
     }
-    s[i] = '\0';        // \0 is the null character that denotes the end of an array.
+    line[i] = '\0';        // \0 is the null character that denotes the end of an array.
     return i;
 }
 
 
 /* copy 'from' into 'to' assuming that 'to' is big enough */
-void copy(char to[], char from[]){
+void copy(void){
     int i;
+    extern char line[], longest[];
 
     i = 0;
-    while((to[i] = from[i]) != '\0')
+    while((longest[i] = line[i]) != '\0')
         ++i;
 }
+
+
+// a usual practice is to collect extern variable declarations and functions in a header
+// and then #include at the top of every source file. 
+// .h suffix for headers is conventional!
+
+// definition: place where variable is created/assigned storage
+//
+// declaration: places where nature of the variable is stated but storage isn't allocated.
